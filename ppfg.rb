4779004cd78f5082940d872bd31a7830712c63f4
xkumiyu <xkumiyu@gmail.com>
2020-11-16 21:07:07 +0900

    update ppfg to 0.2.0

--- ppfg.rb
+++ ppfg.rb
@@ -1,8 +1,8 @@
 class Ppfg < Formula
   desc "Automatically generates python package formula file"
   homepage "https://github.com/xkumiyu/python-package-formula-generater"
-  url "https://github.com/xkumiyu/python-package-formula-generater/archive/0.1.0.tar.gz"
-  sha256 "f1277b79e7038ce2ae08e018e9c0dd165c0b84ddec08c98e24588b00aacf6906"
+  url "https://github.com/xkumiyu/python-package-formula-generater/archive/0.2.0.tar.gz"
+  sha256 "07247b10a4eb9b864ddec5cc581be2de3dc76b859ff6be8e33e8e2f7dd75086c"
 
   def install
     bin.install "bin/ppfg"
