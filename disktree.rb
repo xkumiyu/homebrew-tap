326106a4df33a9ca8f0e9668bdf7bc22fdca98f3
xkumiyu <xkumiyu@gmail.com>
2021-01-06 22:18:06 +0900

    update disktree to 0.2.0

--- disktree.rb
+++ disktree.rb
@@ -1,8 +1,8 @@
 class Disktree < Formula
   desc "DiskTree - a CLI that displays the file size according to directory structure."
   homepage "https://github.com/xkumiyu/disktree"
-  url "https://github.com/xkumiyu/disktree/archive/0.1.0.tar.gz"
-  sha256 "8a215418895012dca791c1aaca24b3c8bc1a5f3e49f2924fe35149859738e43a"
+  url "https://github.com/xkumiyu/disktree/archive/0.2.0.tar.gz"
+  sha256 "36baac46b7828d242d7302241eaa060c42be69356e41edf71d8c03a33cf4b152"
 
   depends_on 'go' => :build
 
